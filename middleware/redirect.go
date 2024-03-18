164696fb8cee653305500fdc584e858b1d3dc923
techoc <techoc@foxmail.com>
2024-03-18 10:22:16 +0800

    :loud_sound:添加重定向日志

--- middleware/redirect.go
+++ middleware/redirect.go
@@ -1,16 +1,17 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func RedirectRules() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 获取请求域名
 		domain := c.Request.Host
-		fmt.Printf("host is %s\n", domain)
 		if domain != "novel.acgh.top" {
+			log.Printf("host is %s\n", domain)
+			log.Printf("will redirect %s\n", "https://novel.acgh.top"+c.Request.URL.String())
 			c.Redirect(301, "https://novel.acgh.top"+c.Request.URL.String())
 		}
 		c.Next()
